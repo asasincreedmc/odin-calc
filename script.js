c9e012abd4cae0def15753fa74ec2679ca93f83b
asasincreedmc <asasincreedmc@gmail.com>
2022-03-17 18:26:02 -0300

    Add basic functionality

--- script.js
+++ script.js
@@ -1,12 +1,46 @@
-let buttons = document.querySelectorAll(".button");
-let displayValue = document.querySelector(".display")
-console.log(buttons)
+const buttons = document.querySelectorAll(".button");
+const displayValue = document.querySelector(".display");
+const specialChars = document.querySelectorAll(".special");
+let num1 = "";
+let num2 = "";
+
+let special = "a"
+let operator = "a"
 for (let button of buttons) {
     button.addEventListener("click", () => {
         displayValue.textContent += button.textContent;
+        checkSpecial()
     })
 }
 
+function checkSpecial() {
+    for (let char of specialChars) {
+        if (displayValue.textContent.includes(`${char.textContent}`)) {
+            if (special === "a") {
+                special = char.textContent;
+                if (special != "C" && special != "=") {
+                    storeData()
+                    displayValue.textContent = ""
+                    console.log({ special })
+                    special = "a"
+                } else if (special == "=") {
+                    storeData()
+                    displayValue.textContent = operate(+num1, operator, +num2)
+                }
+            }
+
+        }
+    }
+}
+
+function storeData() {
+    if (num1 === "") {
+        num1 = displayValue.textContent.slice(0, -1)
+        operator = special;
+    } else if (num2 === "") {
+        num2 = displayValue.textContent.slice(0, -1)
+    }
+}
 
 function operate(num1, operator, num2) {
     if (operator == "+") {
