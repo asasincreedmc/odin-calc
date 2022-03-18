7bd02747a6f4cc37ee7c63ec627acdc22e955bbd
asasincreedmc <asasincreedmc@gmail.com>
2022-03-18 09:57:43 -0300

    Add multiple stringed together operations functionality

--- script.js
+++ script.js
@@ -1,60 +1,94 @@
 const buttons = document.querySelectorAll(".button");
-const displayValue = document.querySelector(".display");
+const displayText = document.querySelector(".display");
+let displayValue = "";
 const specialChars = document.querySelectorAll(".special");
 let num1 = "";
 let num2 = "";
 let special = "a"
 let operator = "a"
+let willOperate = ""
 let result = 0
 for (let button of buttons) {
     button.addEventListener("click", () => {
-        displayValue.textContent += button.textContent;
+        if (willOperate) {
+            displayText.textContent = "";
+            willOperate = false
+        }
+        displayText.textContent += button.textContent;
         checkSpecial()
     })
 }
 
 function checkSpecial() {
     for (let char of specialChars) {
-        if (displayValue.textContent.includes(`${char.textContent}`)) {
+        if (displayText.textContent.includes(`${char.textContent}`)) {
+            special = "a";
             if (special === "a") {
                 special = char.textContent;
                 if (special != "C" && special != "=") {
-                    storeData()
-                    displayValue.textContent = ""
+                    storeData();
+                    changeDisplay();
                 } else if (special == "=") {
-                    storeData()
-                    displayValue.textContent = calculate()
+                    storeData();
+                    changeDisplay();
+
                 } else clearData()
             }
 
         }
     }
 }
+//make num2 on sotredata return calc() and store data so when + is selected again storedata will calculate and then erase all data except result = num1
+function changeDisplay(clear = "") {
+    if ((num2 !== "" && special != "C" && special != "=")) {
+        displayText.textContent = calculate();
+        console.log({ operator })
+        clearData()
+
+    } else if (special == "=") {
+        displayText.textContent = calculate();
+    } else if (special != "C" && special != "=") {
+        displayText.textContent = displayValue;
+    } else if (clear == "C") {
+        displayText.textContent = ""
+    } else displayText.textContent += EventTarget.textContent;
+    displayValue = displayText.textContent;
+
+}
 
 function calculate() {
     result = operate(+num1, operator, +num2)
+    console.log(num1, operator, num2, result)
     return result;
 }
 
 function clearData() {
     if (special == "C") {
         num1 = ""
-    } else num1 = result
+        willOperate = ""
+        operator = "a"
+        changeDisplay("C")
+    } else {
+        num1 = result
+        willOperate = "true"
+        operator = special
+    }
     num2 = "";
     special = "a"
-    operator = "a"
     result = 0
-    displayValue.textContent = ""
 }
 
 function storeData() {
+    displayValue = displayText.textContent.slice(0, -1);
     if (num1 === "") {
-        num1 = displayValue.textContent.slice(0, -1)
+        num1 = displayValue
         operator = special;
+        willOperate = true
     } else if (num2 === "") {
-        num2 = displayValue.textContent.slice(0, -1)
+        num2 = displayValue
+    } else {
+        changeDisplay()
     }
-    special = "a";
 }
 
 function operate(num1, operator, num2) {
@@ -83,6 +117,6 @@ function multiply(num1, num2) {
 
 function divide(num1, num2) {
     if (num2 == 0) {
-        return "OOPS";
+        return "Oops, can't divide by 0";
     } else return num1 / num2;
 }
